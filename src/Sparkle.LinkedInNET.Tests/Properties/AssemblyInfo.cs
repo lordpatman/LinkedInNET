34a847f8b3f95cd25f1651b5080af6b3ec56c2ce
Levente <levente.olah@dirico.io>
2020-01-07 18:51:05 +0100

    Change projects framework to .net 4.7.2. Change projects names.

---
+++ src/Sparkle.LinkedInNET.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sparkle.LinkedInNET.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sparkle.LinkedInNET.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("af7a6bd7-d924-4a21-b3db-995430d61099")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Sparkle.LinkedInNET/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LinkedInNET.ApiV2")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyDescription("LinkedInNET for .NET 4.7.2. Uses linkedIn api v2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c52b4f0b-2bbe-4c81-90c6-019655b66737")]
+
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en")]
